4b53b0901870f81411bac5b917a7598f6721a681
Prince <167101466+pkprajapati7402@users.noreply.github.com>
2024-10-02 11:27:51 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,55 @@
+
+const navbarMenu = document.querySelector(".navbar .links");
+const hamburgerBtn = document.querySelector(".hamburger-btn");
+const hideMenuBtn = navbarMenu.querySelector(".close-btn");
+const showPopupBtn = document.querySelector(".login-btn");
+const formPopup = document.querySelector(".form-popup");
+const hidePopupBtn = formPopup.querySelector(".close-btn");
+const signupLoginLink = formPopup.querySelectorAll(".bottom-link a");
+
+
+
+// AOS.init({
+//     duration:800,
+//     delay:400
+//   });
+
+// Home page
+
+const slider = document.querySelector('.slider');
+
+function activate(e) {
+  const items = document.querySelectorAll('.item');
+  e.target.matches('.next') && slider.append(items[0])
+  e.target.matches('.prev') && slider.prepend(items[items.length-1]);
+}
+
+document.addEventListener('click',activate,false);
+
+
+// Show mobile menu
+hamburgerBtn.addEventListener("click", () => {
+    navbarMenu.classList.toggle("show-menu");
+});
+
+// Hide mobile menu
+hideMenuBtn.addEventListener("click", () =>  hamburgerBtn.click());
+
+// Show login popup
+showPopupBtn.addEventListener("click", () => {
+    document.body.classList.toggle("show-popup");
+});
+
+// Hide login popup
+hidePopupBtn.addEventListener("click", () => showPopupBtn.click());
+
+// Show or hide signup form
+signupLoginLink.forEach(link => {
+    link.addEventListener("click", (e) => {
+        e.preventDefault();
+        formPopup.classList[link.id === 'signup-link' ? 'add' : 'remove']("show-signup");
+    });
+});
+
+// Museum locator
+
---
+++ js/data21.js
@@ -0,0 +1,173 @@
+const museum1 = {
+    id: '66c5b709c4c060f1a1148dc0',
+    museumName: 'National Gallery of Modern Art (New Delhi)', 
+    state: 'Delhi', 
+    description: 'The National Gallery of Modern Art (NGMA) in New Delhi is a premier art gallery that houses an extensive collection of modern and contemporary Indian art. It showcases works by renowned artists such as Raja Ravi Varma, Rabindranath Tagore, and Amrita Sher-Gil.', 
+    img: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.arthistoryproject.com%2Flocations%2Findia%2Fnational-gallery-of-modern-art-new-delhi%2F&psig=AOvVaw16_ZM43Qen_FfzL5_jbUwH&ust=1724319798107000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCNCv7v3lhYgDFQAAAAAdAAAAABAE'
+};
+
+const museum2 = {
+    id: '66c5b71ec4c060f1a1148dc4',
+    museumName: 'Albert Hall Museum (Jaipur, Rajasthan)', 
+    state: 'Rajasthan', 
+    description: 'The Albert Hall Museum, located in Jaipur, Rajasthan, is the oldest museum in the\r\nstate. It is a fine example of Indo-Saracenic architecture and houses a rich collection of artifacts,\r\nincluding paintings, carpets, ivory, stone, metal sculptures, and works in crystal.', 
+    img: 'https://th-thumbnailer.cdn-si-edu.com/KtlP9h1bOjLVVE5wHy6TDe4byv8=/fit-in/1072x0/https://tf-cmsv2-photocontest-smithsonianmag-prod-approved.s3.amazonaws.com/3475206f-e5bb-44db-90f6-25549caaab54.jpeg'
+};
+
+const museum3 = {
+    id: '66c5b7c3c4c060f1a1148dc8',
+    museumName: 'Raja Dinkar Kelkar Museum (Pune, Maharashtra)', 
+    state: 'Maharashtra', 
+    description: 'The Raja Dinkar Kelkar Museum in Pune, Maharashtra, is a one-man collection of artifacts by Dr. Dinkar G. Kelkar. The museum houses a vast collection of\r\neveryday artifacts from various parts of India, including musical instruments, lamps, and textiles.', 
+    img: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fcommons.wikimedia.org%2Fwiki%2FFile%3ABuilding_of_Raja_Dinkar_Kelkar_Museum%2C_Pune.jpg&psig=AOvVaw2NLw0DKJ0RbpzEhpGfG_5F&ust=1724319966953000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCOiIo9HmhYgDFQAAAAAdAAAAABAZ'
+};
+
+const museum4 = {
+    id: '66c5b80bc4c060f1a1148dcc',
+    museumName: 'Archaeological Hampi Museum (Hampi, Karnataka)', 
+    state: 'Karnataka', 
+    description: 'The Archaeological Museum in Hampi, Karnataka, is dedicated to the artifacts and relics found in the ancient city of Hampi, a UNESCO World Heritage Site. The museum showcases sculptures, inscriptions, and other historical artifacts from the Vijayanagara Empire.', 
+    img: 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/09/9b/04/6d/archaeological-museum.jpg?w=1200&h=-1&s=1'
+};
+
+const museum5 = {
+    id: '66c5b823c4c060f1a1148dd0',
+    museumName: 'Bharat Kala Bhavan (Varanasi, Uttar Pradesh)', 
+    state: 'Uttar Pradesh', 
+    description: 'The Calico Museum of Textiles in Ahmedabad, Gujarat, is one of the most\r\ncelebrated institutions of its kind in India. It houses an extraordinary collection of Indian textiles\r\ndating back to the 15th century.', 
+    img: 'https://th.bing.com/th/id/OLC.IPZXi3WhG3rCaA480x360?&rs=1&pid=ImgDetMain'
+};
+
+const museum6 = {
+    id: '66c5b98fc4c060f1a1148dd4',
+    museumName: 'Archaeological Museum (Konark, Odisha)', 
+    state: 'Odisha', 
+    description: 'The Archaeological Museum in Konark, Odisha, is dedicated to the artifacts and sculptures found at the Sun Temple of Konark, a UNESCO\r\nWorld Heritage Site. The museum showcases a collection of sculptures, inscriptions, and other historical artifacts from the site.\r\n', 
+    img: 'https://th.bing.com/th/id/OIP.RDd33k0p59TqHhbXrX6JUQHaE5?rs=1&pid=ImgDetMain'
+};
+const museum7={
+    id:'66c5b1b2f461464a1b55376a',
+    museumName: 'The Calico Museum of Textiles (Ahmedabad, Gujarat)', 
+    state: 'Gujarat', 
+    description: 'The Calico Museum of Textiles in Ahmedabad, Gujarat, is one of the most\r\ncelebrated institutions of its kind in India. It houses an extraordinary collection of Indian textiles dating back to the 15th century.\r\n', 
+    img: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Ftrc-leiden.nl%2Ftrc-needles%2Fcollections%2Fasia%2Fcalico-museum-of-textiles-ahmedabad&psig=AOvVaw2g0SZfH1gx78woF_IS3eM3&ust=1724318339306000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCNjNzMXghYgDFQAAAAAdAAAAABAQ'
+};
+const museum8={
+    id:'66c5b289f461464a1b55376e',
+    museumName: 'The Napier Museum (Thiruvananthapuram, Kerala)', 
+    state: 'Kerala', 
+    description: 'The Napier Museum, located in Thiruvananthapuram, Kerala, is an art and natural history museum. It is known for its unique architectural style, which combines traditional Kerala, Mughal, Chinese, and Italian influences.\r\n', 
+    img: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.napiermuseum.org%2Fhistory&psig=AOvVaw0rq1zBnkhOJMJXfOjsQJWT&ust=1724318649593000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCNiKytjhhYgDFQAAAAAdAAAAABAE'
+};
+const museum9={
+    id:'66c5b296f461464a1b553772',
+    museumName: 'The Indian Museum (Kolkata, West Bengal)', 
+    state: 'West Bengal', 
+    description: 'The Indian Museum in Kolkata is the oldest and largest museum in India. It has\r\nrare collections of antiques, armor, ornaments, fossils, skeletons, mummies, and Mughal\r\npaintings.', 
+    img: 'https://imgstaticcontent.lbb.in/lbbnew/wp-content/uploads/2017/07/24150826/Indian_Museum_Building_with_Quadrangle_-'
+};
+const museum10 = {
+    id: '66c5b51297e308d75e295e29',
+    museumName: 'The Salar Jung Museum (Hyderabad, Telangana)', 
+    state: 'Telangana', 
+    description: 'The Salar Jung Museum is one of the three National Museums of India. It houses\r\nan extensive collection of art, antiques, and artifacts from various parts of the world, including\r\nEurope, Asia, and the Middle East.\r\n', 
+    img: 'https://i.pinimg.com/originals/f9/c6/5e/f9c65e8f78a036b2c27cda726a40db72.jpg'
+};
+const museum11 = {
+    id: '66c5b62793a9b15fb2d55d10',
+    museumName: 'The Government Museum (Chennai, Tamil Nadu)', 
+    state: 'Tamil Nadu', 
+    description: 'The Government Museum in Chennai, also known as the Egmore Museum, is the\r\nsecond oldest museum in India. It has rich collections of archaeological and numismatic\r\nartifacts, Roman antiquities, and a vast collection of sculptures and bronzes.', 
+    img: 'http://casualwalker.com/wp-content/uploads/2019/10/Government_Museum_Chennai16.jpg'
+};
+const museum12 = {
+    id: '66c5b68fc4c060f1a1148dbc',
+    museumName: 'The Jawahar Toy Museum (Puducherry)', 
+    state: 'Puducherry', 
+    description: 'The Jawahar Toy Museum in Puducherry is a small museum dedicated to a collection of dolls and toys from different parts of India. It showcases the cultural diversity and traditions of various Indian states through its exhibits.\r\n', 
+    img: 'https://pondicherrytourism.co.in/images/places-to-visit/header/jawahar-toy-museum-puducherry-tourism-entry-fee-timings-holidays-reviews-header.jpg'
+};
+const museum13 = {
+    id: '66c5ba6ec4c060f1a1148dd8',
+    museumName: 'Khajuraho  Museum (Khajuraho, Madhya Pradesh)', 
+    state: 'Madhya Pradesh', 
+    description: 'The Archaeological Museum in Khajuraho, Madhya Pradesh, is dedicated to the artifacts and sculptures found at the Khajuraho Group of Monuments, a UNESCO World Heritage Site. The museum showcases a collection of sculptures, inscriptions, and other historical artifacts from the site', 
+    img: 'https://www.holidify.com/images/cmsuploads/compressed/shutterstock_1032564361_20200219140243.jpg'
+};
+const museum14 = {
+    id: '66c61f83c4c060f1a1148de1',
+    museumName: 'The Archaeological Museum (Nalanda, Bihar)', 
+    state: 'Bihar', 
+    description: 'The Archaeological Museum in Nalanda, Bihar, is dedicated to the artifacts and\r\n relics found at the ancient Nalanda University site. The museum showcases a collection of\r\n sculptures, inscriptions, and other historical artifacts from the site.', 
+    img: 'https://www.india.com/wp-content/uploads/2014/09/Nalanda.jpg'
+};
+const museum15 = {
+    id: '66c750cdc4c060f1a1148de9',
+    museumName: 'Amaravati Archaeological Museum (Amaravati, Andhra Pradesh)', 
+    state: 'Andhra Pradesh', 
+    description: ' The Archaeological Museum in Amaravati, Andhra Pradesh, is dedicated to the\r\n artifacts and sculptures found at the ancient Buddhist site of Amaravati. The museum\r\n showcases a collection of sculptures, inscriptions, and other historical artifacts from the site\r\n.', 
+    img: 'https://lh5.googleusercontent.com/p/AF1QipN8nszS4pQm40MLwIR9GC6Zhu8UlqTskJ42E7EK=w1080-k-no'
+};
+const museum16 = {
+    id: '66c759f3c4c060f1a1148dfd',
+    museumName: 'The Archaeological Museum (Chandigarh, Punjab)', 
+    state: 'Punjab', 
+    description: 'The Archaeological Museum in Chandigarh is dedicated to the artifacts and relics found in the region. The museum showcases a collection of sculptures, inscriptions, and other historical artifacts from various sites in Chandigarh and surrounding areas.', 
+    img: 'http://upload.wikimedia.org/wikipedia/commons/4/49/Chandigarh_Museum.jpg'
+};
+const museum17 = {
+    id: '66c763bb9d433c5ce428030d',
+    museumName: 'The Archaeological Museum (Goa)', 
+    state: 'Goa', 
+    description: 'The Archaeological Museum in Goa is dedicated to the artifacts and relics found in the state of Goa. The museum showcases a collection of sculptures, inscriptions, and other historical artifacts from various\r\n sites in Goa.', 
+    img: 'https://i.pinimg.com/originals/bf/d5/91/bfd591294100ca3e0892076b6e45422f.jpg'
+};
+const museum18 = {
+    id: '66c777d12cfc822d154629c6',
+    museumName: 'Archaeological Site Museum Kangra (Kangra, Himachal Pradesh)', 
+    state: 'Himachal Pradesh', 
+    description: 'The Kangra Fort is a historic fort located in the Kangra district of the Himachal Pradesh state in India. The fort is also known as "Nagarkot" and "Kot Kangra". This fort stands on a hillock between two rivers (Manjhi and Banganga), among the foothills of the Dhauladhar range. The fort is the largest in the Indian Himalayas, and is under the protection of the Archeological Survey of India.', 
+    img: 'https://lh3.googleusercontent.com/p/AF1QipOI1x-Dcmgk0IcW3wDbd25v2yPQd5zC-if6I9Pu=w1080-h608-p-no-v0'
+};
+const museum19 = {
+    id: '66c7798e2cfc822d154629ca',
+    museumName: 'The Archaeological Museum (Jageshwar, Uttarakhand)', 
+    state: 'Uttarakhand', 
+    description: 'The Archaeological Museum in Jageshwar, Uttarakhand, is a significant repository of historical and cultural artifacts, primarily related to the ancient Jageshwar temple complex, one of the most important Hindu pilgrimage sites dedicated to Lord Shiva.', 
+    img: 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0e/b8/d7/22/jageshwar-dham-museum.jpg?w=1200&h=1200&s=1'
+};
+const museum20 = {
+    id: '66c77b7c2cfc822d154629ce',
+    museumName: 'The Archaeological Museum (Kurukshetra, Haryana)', 
+    state: 'Haryana', 
+    description: 'The Archaeological Museum in Thaneswar (also known as the Thaneswar Archaeological Museum) is located in the town of Thaneswar, in the Kurukshetra district of Haryana, India. It is an important museum for understanding the rich historical and cultural heritage of the region', 
+    img: 'https://www.incredibleindia.org/content/dam/incredible-india-v2/images/places/kurukshetra/thanesar-archaeological-site-museum-kurukshetra-haryana.jpg/jcr:content/renditions/cq5dam.web.1200.491.jpeg'
+};
+const museum21 = {
+    id: '66c77ccd2cfc822d154629d2',
+    museumName: 'The Sri Suryapahar Museum (Baksa, Assam)', 
+    state: 'Assam', 
+    description: 'The Sri Suryapahar Museum, located in Suryapahar, Baksa District, Assam, is a specialized museum dedicated to preserving and showcasing the archaeological and historical heritage of the Suryapahar site.', 
+    img: 'https://travellingslacker.com/wp-content/uploads/2020/03/Sri-Surya-6.jpg'
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Optionally, group them into an array or object
+const museums = [museum1, museum2, museum3, museum4, museum5, museum6,museum7,museum8,museum9,museum10, museum11,museum12, museum13,museum14,museum15,museum16,museum17,museum18,museum19,museum20,museum21]; // An array of objects
+
+// Export the objects
+export { museum1, museum2, museum3, museum4, museum5,museum6,museum7,museum8,museum9,museum10, museum11,museum12, museum13,museum14,museum15,museum16,museum17,museum18,museum19,museum20,museum21,museums };
+
+
+
